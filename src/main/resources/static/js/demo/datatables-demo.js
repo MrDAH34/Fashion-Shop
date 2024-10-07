f49e348c4a45c5187b5bc6f37a7644130164365e
MrDAH34 <anhhao22082004@gmail.com>
2024-10-07 22:51:38 +0700

    delete sort, some demo function

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -1,4 +1,8 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+      "columnDefs": [
+          { "orderable": false, "targets": [0,4,6,7,8,9] } 
+      ]
+  });
 });
